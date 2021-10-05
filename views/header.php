f52d50a7a989ea2324eedc5eab41e3a1f1c12338
Tigran Barseghyan <t_barseghyan@yahoo.com>
2021-10-05 11:34:44 +0400

    Add header

--- views/header.php
+++ views/header.php
@@ -4,6 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
+    <title>The Best Online Market Ever</title>
 </head>
 <body>
